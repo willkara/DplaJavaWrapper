0b99370be8f8e27116c4f932c4230f683e0e2296
William Karavites <wkaravites@gmail.com>
2013-04-24 13:42:34 -0400

    HouseKeeping
    Fixed the README file and changed it to README.md so it is readable as
    markdown and changed the license.

---
+++ README.md
@@ -0,0 +1,67 @@
+# Java Wrapper for the Digital Public Library's API
+
+
+Javadoc available at: http://rci.rutgers.edu/~willkara/apidocs/
+
+
+This is meant to be a simple Java wrapper for the Digital Public Library's API 
+
+http://dp.la
+
+https://github.com/dpla/platform
+
+
+
+
+## Examples
+--------------------------------
+
+Before anything is done you must set your apikey.
+
+
+    SearchQuery sq = new SearchQuery("pizza",null);
+        
+    sq.setAPIKEY("your api key goes here");
+
+
+
+Every SearchQuery will return an array full of DplaItems. You can then gain access to each items information from there.
+
+You can execute a search with:
+
+* Just a Search Query
+* Search Query & SearchOptions
+* Just SearchOptions
+
+If you only want one, then just pass null for the other.
+
+### Execute a simple search query WITHOUT any SearchOptions.
+'''Java
+    SearchQuery sq = new SearchQuery("pizza",null);
+        
+        sq.setAPIKEY("your api key goes here");
+        DplaItem[] result = sq.search();
+
+        for (DplaItem dp : result) {
+            System.out.println(dp.getSourceResource().getTitle());
+        }
+'''
+### Execute a search query with some SearchOptions
+
+'''java
+    
+    SearchOptions so = new SearchOptions();
+
+    so.description="history";
+    SearchQuery sq = new SearchQuery("pizza",null);
+        
+        sq.setAPIKEY("your api key goes here");
+        DplaItem[] result = sq.search();
+
+        for (DplaItem dp : result) {
+            System.out.println(dp.getSourceResource().getTitle());
+        }
+'''
+
+
+
--- src/main/java/willkara/dplajavawrapper/SearchQuery.java
+++ src/main/java/willkara/dplajavawrapper/SearchQuery.java
@@ -6,19 +6,27 @@
  * I just ask that you attirbute the contributions of myself and other developers.
  * 
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * The MIT License (MIT)
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * Copyright (c) <2013> <William Karavites>
  *
- * You should have received a copy of the GNU Affero General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/>.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package willkara.dplajavawrapper;
 
@@ -63,14 +71,14 @@ public class SearchQuery {
 
     }
 
-    
     /**
-     * You must run this method with your api_key first before executing any queries
-     * 
-     * If you do not have an api_key yet please run this command:
-     * curl -v -XPOST http://api.dp.la/v2/api_key/you@your_email.com
-     * 
-     * 
+     * You must run this method with your api_key first before executing any
+     * queries
+     *
+     * If you do not have an api_key yet please run this command: curl -v -XPOST
+     * http://api.dp.la/v2/api_key/you@your_email.com
+     *
+     *
      * @param key Your api_key
      */
     public void setAPIKEY(String key) {
@@ -87,7 +95,7 @@ public class SearchQuery {
     public DplaItem[] search() throws IOException {
         String queryURL = "";
         String durl;
-        
+
         //search using JUST a search query
         if (so == null && searchQuery != null) {
 
